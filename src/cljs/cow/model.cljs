a8f66d13368ac9620098c8bcb33eb7fc70b9db09
Tim Berglund <tlberglund@github.com>
2013-04-06 18:46:52 -0600

    Refactored trig functions

--- src/cljs/cow/model.cljs
+++ src/cljs/cow/model.cljs
@@ -4,24 +4,24 @@
             [goog.Timer]))
 
 (def cow-count 20)
-(def pi 3.1415926535)
 
+(defn polar-to-rect [theta radius]
+  [(* radius (Math/cos theta)) (* radius (Math/sin theta))])
 
-(defn x-from-polar [theta radius]
-  (* radius (Math/cos theta)))
+(defn square [x] (* x x))
 
-(defn y-from-polar [theta radius]
-  (* radius (Math/sin theta)))
+(defn hypotenuse [x y] 
+  (sqrt (+ (square x) (square y))))
 
 (defn random-cow []
-  (let [theta (- (* 2 pi) (rand (* 4 pi)))
+  (let [theta (- (* 2 Math/PI) (rand (* 4 Math/PI)))
         radius (rand)
         cow (atom {
         :anxiety 0
-        :angle (- (* 2 pi) (rand (* 4 pi)))
+        :angle (- (* 2 Math/PI) (rand (* 4 Math/PI)))
         :velocity (rand 0.01)
-        :x (x-from-polar theta radius)
-        :y (y-from-polar theta radius)
+        :x ((polar-to-rect theta radius) 0)
+        :y ((polar-to-rect theta radius) 1)
         :self-differentiation (rand)
         })]
     cow)
@@ -29,16 +29,7 @@
 
 (def canvas (dom/get-element "model"))
 (def timer (goog.Timer. (/ 1000 20)))
-
 (def cows (doall (take cow-count (repeatedly random-cow))))
-
-
-
-(defn sim-cows [cows]
-  (doseq [cow cows]
-    (let [new-x (+ (:x @cow) (x-from-polar (:angle @cow) (:velocity @cow)))
-          new-y (+ (:y @cow) (y-from-polar (:angle @cow) (:velocity @cow)))]
-      (swap! cow assoc :x new-x :y new-y))))
 
 (defn init-canvas [canvas]
   (let [ctx (.getContext canvas "2d")
@@ -47,8 +38,24 @@
     (do
       (.clearRect ctx 0 0 width height)
       (.beginPath ctx)
-      (.arc ctx (/ width 2) (/ height 2) (/ width 2) 0 (* 2 pi) false)
+      (.arc ctx (/ width 2) (/ height 2) (/ width 2) 0 (* 2 Math/PI) false)
       (.stroke ctx))))
+
+(defn hit-fence? [canvas cow]
+  (let [width (.getAttribute canvas "width")
+        fence-radius (/ width 2)
+        cow-radius (hypotenuse (:x cow) (:y cow))]
+    (> cow-radius fence-radius)))
+
+(def incident-angle [canvas cow]
+
+  )
+
+(defn sim-cows [cows]
+  (doseq [cow cows]
+    (let [new-x (+ (:x @cow) ((polar-to-rect (:angle @cow) (:velocity @cow)) 0))
+          new-y (+ (:y @cow) ((polar-to-rect (:angle @cow) (:velocity @cow)) 1))]
+      (swap! cow assoc :x new-x :y new-y))))
 
 (defn paint-cow [canvas cow]
   (let [ctx (.getContext canvas "2d")
@@ -74,3 +81,4 @@
 
 (event/listen timer goog.Timer/TICK cow-sim)
 (.start timer)
+(.log js/console cows)
