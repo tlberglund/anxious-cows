813c725d441a6e6b7f0beab45bd3fae3d5c11380
Tim Berglund <tlberglund@github.com>
2013-09-09 18:44:58 +0200

    Simulation progress.
    * Anxiety decay
    * Render cows as circles with color and radius proportional to anxiety
    * Break out math code into its own library

--- src/cljs/cow/math.cljs
+++ src/cljs/cow/math.cljs
@@ -1,2 +1,24 @@
-(ns cow)  
+(ns cow.math)
 
+
+(defn polar-to-rect 
+  ([v] (apply polar-to-rect v))
+  ([theta radius]
+    [(* radius (Math/cos theta)) (* radius (Math/sin theta))]))
+
+(defn square [x] (* x x))
+
+(defn hypotenuse
+  ([x y] (Math/sqrt (+ (square x) (square y))))
+  ([v] (apply hypotenuse v)))
+
+(defn rect-to-polar
+  ([v] (apply rect-to-polar v))
+  ([x y]
+    [(Math/atan (/ y x)) (hypotenuse x y)]))
+
+(defn rand-normal [mean stddev]
+  (+ mean (* (- (/ (reduce + (repeatedly 10 rand)) 10) 0.5) stddev)))
+
+(defn degrees-to-radians [deg]
+  (* (/ deg 180) Math/PI))
--- src/cljs/cow/model.cljs
+++ src/cljs/cow/model.cljs
@@ -1,45 +1,23 @@
-(ns cow
+(ns cow.model
   (:require [clojure.browser.event :as event]
             [clojure.browser.dom :as dom]
             [goog.Timer]
-            [crate.core :as crate])
-  (:use [domina :only [by-id set-html! set-text! set-value!]]))
+            [crate.core :as crate]
+            [cow.math :as math])
+  (:use [domina :only [by-id set-html! set-text! set-value! set-style!]]))
 
-(def cow-count 5)
+(def cow-count 10)
 (def max-starting-velocity 0.01)
 (def max-turn-degrees 180)
 
 
-(defn polar-to-rect 
-  ([v] (apply polar-to-rect v))
-  ([theta radius]
-    [(* radius (Math/cos theta)) (* radius (Math/sin theta))]))
-
-(defn square [x] (* x x))
-
-(defn hypotenuse
-  ([x y] (Math/sqrt (+ (square x) (square y))))
-  ([v] (apply hypotenuse v)))
-
-(defn rect-to-polar
-  ([v] (apply rect-to-polar v))
-  ([x y]
-    [(Math/atan (/ y x)) (hypotenuse x y)]))
-
-(defn rand-normal [mean stddev]
-  (+ mean (* (- (/ (reduce + (repeatedly 10 rand)) 10) 0.5) stddev)))
-
-(defn degrees-to-radians [deg]
-  (* (/ deg 180) Math/PI))
-
 (def cow-id (atom 0))
 (defn random-cow []
   (let [random-velocity (fn [] (- max-starting-velocity (rand (* 2 max-starting-velocity))))
-        ; random-position (fn [] (- 1 (rand 2)))
-        random-position (fn [] (rand-normal 0 1))
+        random-position (fn [] (math/rand-normal 0 1))
         cow (atom {
                    :id (swap! cow-id inc)
-                   :anxiety 0
+                   :anxiety (rand)
                    :velocity [(random-velocity) (random-velocity)]
                    :pos [(random-position) (random-position)]
                    :self-differentiation (rand)
@@ -70,39 +48,36 @@
   ([x y] (every? #(> (Math/abs %) 1) (vector x y))))
 
 
-; (defn perturb-vector [velocity heading]
-;   (let [polar (rect-to-polar velocity)
-;         perturbed-polar (assoc polar 0 (+ (polar 0) heading))]
-;     (polar-to-rect polar)))
 (defn perturb-vector [velocity heading]
-  (let [polar (rect-to-polar velocity)
+  (let [polar (math/rect-to-polar velocity)
         perturbed-polar (assoc polar 0 (+ (polar 0) heading))]
-    (polar-to-rect polar)))
+    ; (.log js/console (str (polar-to-rect polar) " | " velocity))
+    ; Disable random walking for now
+    ; (polar-to-rect perturbed-polar)))
+    velocity))
 
 
 (defn random-walk [cow]
   (let [velocity (:velocity cow)
         vx (velocity 0)
         vy (velocity 1)
-        random-x (rand-normal vx (* 2 max-starting-velocity))
-        random-y (rand-normal vy (* 2 max-starting-velocity))
+        random-x (math/rand-normal vx (* 2 max-starting-velocity))
+        random-y (math/rand-normal vy (* 2 max-starting-velocity))
         heading (Math/atan (/ vy vx))
-        wander (* (rand-normal 0 (degrees-to-radians max-turn-degrees)))]
+        wander (* (math/rand-normal 0 (math/degrees-to-radians max-turn-degrees)))]
     (perturb-vector (:velocity cow) wander)))
-    ; (:velocity cow)))
-    ; [random-x random-y]))
 
 (defn new-cow-velocity [cow]
   (let [velocity (:velocity cow)
         pos (:pos cow)
         negate (fn [vec n] (assoc vec n (- (vec n))))]
     (cond 
-      (> (Math/abs (pos 0)) 0.99) (negate velocity 0)
-      (> (Math/abs (pos 1)) 0.99) (negate velocity 1)
+      (> (Math/abs (pos 0)) 0.97) (negate velocity 0)
+      (> (Math/abs (pos 1)) 0.97) (negate velocity 1)
       :else (random-walk cow))))
-      ; :else (:velocity cow))))
 
-(defn new-cow-anxiety [cows cow] ())
+(defn new-cow-anxiety [cows cow] 
+  (* (:anxiety cow) 0.99))
 
 (defn sim-cows [cows]
   (doseq [cow-atom cows]
@@ -116,19 +91,28 @@
              :velocity new-velocity))))
 
 (defn draw-box 
-  ([ctx position width height]
+  ([ctx position width height fill-style]
      (let [half-width (/ width 2)
            half-height (/ height 2)
            upper-left (- (position 0) half-width)
            upper-right (- (position 1) half-height)]
        (do 
          (.beginPath ctx)
+         (set! (. ctx -fillStyle) fill-style)
          (.fillRect ctx upper-left upper-right width height)
          (.closePath ctx))))
-  ([ctx position width]
-     (draw-box ctx position width width))
-  ([ctx position]
-     (draw-box ctx position 5)))
+  ([ctx position width fill-style]
+     (draw-box ctx position width width fill-style))
+  ([ctx position fill-style]
+     (draw-box ctx position 5 fill-style)))
+
+(defn draw-circle [ctx position radius fill-style]
+  (do
+    (set! (. ctx -fillStyle) fill-style)
+    (.beginPath ctx)
+    (.arc ctx (position 0) (position 1) radius 0 (* 2 Math/PI) true)
+    (.closePath ctx)
+    (.fill ctx)))
 
 (defn cow-to-canvas-coord [canvas-dim cow-coord]
   (let [dimension (/ canvas-dim 2)]
@@ -137,8 +121,11 @@
 (defn paint-cow [canvas cow]
   (let [ctx (.getContext canvas "2d")
         ctx-size (vec (map #(.getAttribute canvas %1) ["width" "height"]))
-        ctx-pos (vec (map cow-to-canvas-coord ctx-size (:pos cow)))]
-    (draw-box ctx ctx-pos)))
+        ctx-pos (vec (map cow-to-canvas-coord ctx-size (:pos cow)))
+        cow-color (str "rgb(" (format "%d" (int (* 256 (:anxiety cow)))) ",0,0)")
+        cow-size (int (+ 5 (* 15 (:anxiety cow))))]
+    ; (draw-box ctx ctx-pos cow-size cow-color)))
+    (draw-circle ctx ctx-pos cow-size cow-color)))
 
 (defn paint-sim [canvas cows]
   (do 
@@ -146,25 +133,15 @@
     (doseq [cow cows]
       (paint-cow canvas @cow))))
 
-(defn update-cow-debug [element cows]
-  (letfn [(render-pos [cow] (:pos cow))
-          (render-velocity [cow] (:velocity cow))
-          (render-anxiety [cow] (:anxiety cow))
-          (render-sd [cow] (:self-differentiation cow))
-          (cow-parms [cow] (map #(cow %) [:pos :velocity :anxiety :self-differentiation]))
-          (cow-cells [cow] (apply str (map #(dom/element "td" (str %)) (cow-parms cow))))
-          (cow-row [cow] (dom/element "tr" (cow-cells cow)))]
-      (let [rows (map #(cow-row @%) cows)]
-        (dom/append element rows))))
-
 (defn cow-table [cows]
   (let [row-count (atom 0)
         format-num (fn [num] (format "%1.04f" num))
         format-pair (fn [vec] (str "(" (apply str (interpose ", " (map format-num vec))) ")"))
         render-pos (fn [cow] (format-pair (:pos cow)))
         render-velocity (fn [cow] (format-pair (:velocity cow)))
-        render-anxiety (fn [cow] (:anxiety cow))
+        render-anxiety (fn [cow] (format-num (:anxiety cow)))
         render-sd (fn [cow] (format-num (:self-differentiation cow)))
+        td-id (fn [prefix] (str prefix @row-count))
     ]
     (crate/html 
       [:table#cows {:width "100%"}
@@ -175,10 +152,10 @@
           (map
             #(crate/html [:tr {:id (str "cow-row-" (swap! row-count inc))}
               [:td (str (:id %))]
-              [:td {:id (str "cow-position-" @row-count)} (render-pos %)]
-              [:td {:id (str "cow-velocity-" @row-count)} (render-velocity %)]
-              [:td {:id (str "cow-anxiety-" @row-count)} (render-anxiety %)]
-              [:td {:id (str "cow-sd-" @row-count)} (render-sd %)]])
+              [:td {:id (td-id "cow-position-")} (render-pos %)]
+              [:td {:id (td-id "cow-velocity-")} (render-velocity %)]
+              [:td {:id (td-id "cow-anxiety-")} (render-anxiety %)]
+              [:td {:id (td-id "cow-sd-")} (render-sd %)]])
             (map deref cows))
         ]
       ])))
@@ -193,12 +170,13 @@
     (sim-cows cows)
     (update-params cows)
     (paint-sim canvas cows)))
-
               
 (def timer (goog.Timer. (/ 1000 20)))
 (event/listen (by-id "run-sim") goog.events.EventType.CLICK #(.start timer))
 (event/listen (by-id "stop-sim") goog.events.EventType.CLICK #(.stop timer))
 (event/listen (by-id "step-sim") goog.events.EventType.CLICK #(cow-sim))
 (event/listen timer goog.Timer/TICK cow-sim)
-(.start timer)
+(update-params cows)
+(paint-sim canvas cows)
+; (.start timer)
 
