b989622f2562758f1651e8b7c1a4729de92dbc2b
Tim Berglund <tlberglund@github.com>
2013-09-09 18:53:45 +0200

    Factored out UI code into its own lib

--- src/cljs/cow/model.cljs
+++ src/cljs/cow/model.cljs
@@ -1,10 +1,5 @@
 (ns cow.model
-  (:require [clojure.browser.event :as event]
-            [clojure.browser.dom :as dom]
-            [goog.Timer]
-            [crate.core :as crate]
-            [cow.math :as math])
-  (:use [domina :only [by-id set-html! set-text! set-value! set-style!]]))
+  (:require [cow.math :as math]))
 
 (def cow-count 10)
 (def max-starting-velocity 0.01)
@@ -24,7 +19,6 @@
                    })]
     cow))
 
-(def canvas (dom/get-element "model"))
 (def cows (repeatedly cow-count random-cow))
 
 (defn init-canvas [canvas]
@@ -133,50 +127,6 @@
     (doseq [cow cows]
       (paint-cow canvas @cow))))
 
-(defn cow-table [cows]
-  (let [row-count (atom 0)
-        format-num (fn [num] (format "%1.04f" num))
-        format-pair (fn [vec] (str "(" (apply str (interpose ", " (map format-num vec))) ")"))
-        render-pos (fn [cow] (format-pair (:pos cow)))
-        render-velocity (fn [cow] (format-pair (:velocity cow)))
-        render-anxiety (fn [cow] (format-num (:anxiety cow)))
-        render-sd (fn [cow] (format-num (:self-differentiation cow)))
-        td-id (fn [prefix] (str prefix @row-count))
-    ]
-    (crate/html 
-      [:table#cows {:width "100%"}
-        [:thead 
-          [:tr [:th "Cow"] [:th "Position"] [:th "Velocity"] [:th "Anxiety"] [:th "SD"]]
-        ]
-        [:tbody 
-          (map
-            #(crate/html [:tr {:id (str "cow-row-" (swap! row-count inc))}
-              [:td (str (:id %))]
-              [:td {:id (td-id "cow-position-")} (render-pos %)]
-              [:td {:id (td-id "cow-velocity-")} (render-velocity %)]
-              [:td {:id (td-id "cow-anxiety-")} (render-anxiety %)]
-              [:td {:id (td-id "cow-sd-")} (render-sd %)]])
-            (map deref cows))
-        ]
-      ])))
-
-(defn update-params [cows]
-  (set-text! (by-id "elapsed-time") (js/Date.))
-  (set-html! (by-id "cow-table") (cow-table cows)))
 
 
-(defn cow-sim []
-  (do
-    (sim-cows cows)
-    (update-params cows)
-    (paint-sim canvas cows)))
               
-(def timer (goog.Timer. (/ 1000 20)))
-(event/listen (by-id "run-sim") goog.events.EventType.CLICK #(.start timer))
-(event/listen (by-id "stop-sim") goog.events.EventType.CLICK #(.stop timer))
-(event/listen (by-id "step-sim") goog.events.EventType.CLICK #(cow-sim))
-(event/listen timer goog.Timer/TICK cow-sim)
-(update-params cows)
-(paint-sim canvas cows)
-; (.start timer)
-
