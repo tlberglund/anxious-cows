cea4b15f9bde129cbff603748b7324b63cd776e9
Tim Berglund <tlberglund@github.com>
2013-09-11 12:46:00 +0200

    Cows move around and bounce off walls

--- src/cljs/cow/model.cljs
+++ src/cljs/cow/model.cljs
@@ -9,9 +9,27 @@
         random-position (fn [] (- 0.95 (rand 1.9)))
         cow (atom {
                    :id (swap! cow-id inc)
+                   :velocity [(random-velocity) (random-velocity)]
                    :pos [(random-position) (random-position)]
                    })]
     cow))
 
 (def cows (repeatedly cow-count random-cow))
 
+(defn new-cow-velocity [cow]
+  (let [velocity (:velocity cow)
+        pos (:pos cow)
+        negate (fn [vec n] (assoc vec n (- (vec n))))]
+    (cond
+      (> (Math/abs (pos 0)) 0.97) (negate velocity 0)
+      (> (Math/abs (pos 1)) 0.97) (negate velocity 1)
+      :else velocity)))
+
+(defn sim-cows [cows]
+  (doseq [cow-atom cows]
+    (let [cow @cow-atom
+          new-velocity (new-cow-velocity cow)
+          new-pos (vec (map + new-velocity (:pos cow)))]
+      (swap! cow-atom assoc
+             :pos new-pos
+             :velocity new-velocity))))
--- src/cljs/cow/ui.cljs
+++ src/cljs/cow/ui.cljs
@@ -1,32 +1,43 @@
 (ns cow.ui
-  (:require [crate.core :as crate])
-  (:use [domina :only [by-id set-html!]]
-        [cow.model :only [cows]]))
+  (:require [clojure.browser.event :as event]
+            [goog.Timer]
+            [crate.core :as crate])
+  (:use [domina :only [by-id set-html! set-text!]]
+        [cow.model :only [cows sim-cows]]))
 
 (def canvas (by-id "cow-pen"))
-
+(def refresh-table (atom true))
+(def display-ids (atom false))
 
 (defn cow-table [cows]
   (let [row-count (atom 0)
         format-num (fn [num] (format "%1.04f" num))
         format-pair (fn [vec] (str "(" (apply str (interpose ", " (map format-num vec))) ")"))
+        render-velocity (fn [cow] (format-pair (:velocity cow)))
         render-pos (fn [cow] (format-pair (:pos cow)))
         td-id (fn [prefix] (str prefix @row-count))
        ]
     (crate/html 
       [:table#cows {:width "100%"}
         [:thead 
-          [:tr [:th "Cow"] [:th "Position"]]
+          [:tr [:th "Cow"] [:th "Position"] [:th "Velocity"]]
         ]
         [:tbody 
           (map
             #(crate/html [:tr {:id (str "cow-row-" (swap! row-count inc))}
               [:td (str (:id %))]
               [:td {:id (td-id "cow-position-")} (render-pos %)]
+              [:td {:id (td-id "cow-velocity-")} (render-velocity %)]
               ])
             (map deref cows))
         ]
       ])))
+
+(defn update-params [cows]
+  (set-text! (by-id "elapsed-time") (js/Date.))
+  (if @refresh-table
+      (set-html! (by-id "cow-table") (cow-table cows)))
+  )
 
 (defn init-canvas [canvas]
   (let [ctx (.getContext canvas "2d")
@@ -62,7 +73,12 @@
         cow-color "#000000"
         cow-size 10]
     (do
-      (draw-circle ctx ctx-pos cow-size cow-color))))
+      (draw-circle ctx ctx-pos cow-size cow-color)
+      (if @display-ids (do
+        (set! (. ctx -fillStyle) "#303030")
+        (set! (. ctx -font) "8pt Arial")
+        (.fillText ctx (str (:id cow)) (+ (ctx-pos 0) 10) (+ (ctx-pos 1) 4))))
+    )))
 
 (defn paint-sim [canvas cows]
   (do 
@@ -70,8 +86,36 @@
     (doseq [cow cows]
       (paint-cow canvas @cow))))
 
+(def frame-count (atom 0))
+
+(defn cow-sim []
+  (do
+    (swap! frame-count inc)
+    (sim-cows cows)
+    (update-params cows)
+    (paint-sim canvas cows)))
+
+(defn update-fps []
+  (set-text! (by-id "fps") (str @frame-count))
+  (swap! frame-count (fn [] 0)))
+
+
+(def sim-timer (goog.Timer. (/ 1000 20)))
+(def fps-timer (goog.Timer. 1000))
+(event/listen (by-id "run-sim") goog.events.EventType.CLICK #(.start sim-timer))
+(event/listen (by-id "stop-sim") goog.events.EventType.CLICK #(.stop sim-timer))
+(event/listen (by-id "step-sim") goog.events.EventType.CLICK #(cow-sim))
+(event/listen sim-timer goog.Timer/TICK cow-sim)
+(event/listen fps-timer goog.Timer/TICK update-fps)
+(event/listen (by-id "chk-refresh-table")
+              goog.events.EventType.CLICK
+              #(swap! refresh-table not))
+(event/listen (by-id "chk-display-ids")
+              goog.events.EventType.CLICK
+              #(swap! display-ids not))
+(update-params cows)
 (paint-sim canvas cows)
-(set-html! (by-id "cow-table") (cow-table cows))
+(.start fps-timer)
 
 
 
